52ed57319dc81f4d589e02be61b22b476516a2cd
fuhanwen19930816@gmail.com <fuhanwen83083705>
2018-01-18 18:02:51 +0800

    int

--- src/js/header.js
+++ src/js/header.js
@@ -1,13 +1,12 @@
 import React from 'react';
 /*import ReactDOM from 'react-dom';*/
-import logo from '../logo.svg';
 import './App.css';
 
 export default class ComponentHeader extends React.Component{
   render() {
     return(
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src='https://gw.alicdn.com/tfs/TB1MgGBnvDH8KJjy1XcXXcpdXXa-16-16.ico' className="App-logo" alt="logo" />
         <h1 className="App-title">My name is fuhanwen</h1>
         <h2 className="App-title">This is my resume.</h2>
       </header>
